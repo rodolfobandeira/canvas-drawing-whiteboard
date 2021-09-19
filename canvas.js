027f33285ae32c44f792e017468f795f71729d58
rodolfobandeira <contato@rodolfobandeira.com>
2021-09-19 10:57:33 -0400

    Start work to save coordinates as vectors in the database

--- canvas.js
+++ canvas.js
@@ -6,8 +6,9 @@ window.addEventListener("load", () => {
     canvas.width = window.innerWidth - 10;
 
     let painting = false;
+    let points = [];
 
-    function startPosition() {
+    function startPosition(e) {
         painting = true;
         draw(e);
     }
@@ -15,9 +16,18 @@ window.addEventListener("load", () => {
     function finishedPosition() {
         painting = false;
         context.beginPath();
+        // console.log(points);
+        renderPoints(points);
+    }
+
+    function getXY(e) {
+      let r = canvas.getBoundingClientRect();
+      return {x: e.clientX - r.left, y: e.clientY - r.top}
     }
 
     function draw(e) {
+        let pos = getXY(e);
+
         if (!painting) return;
         context.lineWidth = 4;
         context.lineCap = "round";
@@ -28,9 +38,30 @@ window.addEventListener("load", () => {
         context.beginPath();
         context.moveTo(e.clientX, e.clientY);
 
+        points = [];
+        points[points.length - 1].push([pos.x, pos.y]);
+        // console.log(pos);
     }
 
-    canvas.addEventListener("mousedown", startPosition);
-    canvas.addEventListener("mouseup", finishedPosition);
-    canvas.addEventListener("mousemove", draw);
-});
+    function renderPoints(points) {
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.strokeStyle = '#FF0000';
+        context.beginPath();
+
+        /// get a stroke
+        for(let i = 0; i < points.length; i++) {
+            console.log(points[i][0], points[i][1]);
+
+            context.lineTo(points[i][0], points[i][1]);
+            context.stroke();
+            context.beginPath();
+            context.moveTo(points[i][0], points[i][1]);
+        }
+    }
+
+      canvas.addEventListener("mousedown", startPosition);
+      canvas.addEventListener("mouseup", finishedPosition);
+      canvas.addEventListener("mousemove", draw);
+  });
+
