20810925ffd4ea91d874aa3ed02bb9eae7472b42
rodolfobandeira <contato@rodolfobandeira.com>
2021-09-19 19:33:46 -0400

    Messing around

--- canvas.js
+++ canvas.js
@@ -1,4 +1,24 @@
-function renderPoints(context, points) {
+window.addEventListener("load", () => {
+  let painting = false;
+  let points = [];
+  const canvas = document.querySelector('#canvas');
+  const context = canvas.getContext('2d');
+  canvas.height = window.innerHeight - 10;
+  canvas.width = window.innerWidth - 10;
+
+  storageFromPoints = localStorage.getItem("points");
+  if ("" !== storageFromPoints) {
+    renderPoints(
+      context, JSON.parse(storageFromPoints)
+    );
+  }
+
+
+  function renderPoints(points) {
+    if (null === points) {
+      return;
+    }
+
     context.lineWidth = 3;
     context.strokeStyle = '#FF0000';
     context.lineCap = "round";
@@ -12,68 +32,67 @@ function renderPoints(context, points) {
     context.beginPath();
 
     points.forEach(point => {
-        xx = point[0][0];
-        yy = point[0][1];
-        context.lineTo(xx, yy);
+      xx = point[0][0];
+      yy = point[0][1];
+      context.lineTo(xx, yy);
     });
 
     context.stroke();
-}
+  }
 
 
-window.addEventListener("load", () => {
-    const canvas = document.querySelector('#canvas');
-    const context = canvas.getContext('2d');
+  function loadScreen(pts) {
+    console.log("load screen...");
+    renderPoints(pts);
+  }
 
-    canvas.height = window.innerHeight - 10;
-    canvas.width = window.innerWidth - 10;
+  function saveScreen() {
+    console.log("save screen...");
+  }
 
-    // localStorage.setItem("points", []);
-    pp = JSON.parse(localStorage.getItem("points"))
-    console.log(pp);
-    renderPoints(context, pp);
+  function clearScreen() {
+    console.log("cleaning screen and storage...");
+    localStorage.removeItem("points");
+  }
 
-    let painting = false;
-    let points = [];
+  function startPosition(e) {
+    painting = true;
+    draw(e);
+  }
 
-    function startPosition(e) {
-        painting = true;
-        draw(e);
-    }
+  function finishedPosition() {
+    painting = false;
+    context.beginPath();
+    // console.log(points);
+    // renderPoints(points);
+  }
 
-    function finishedPosition() {
-        painting = false;
-        context.beginPath();
-        // console.log(points);
-        // renderPoints(points);
-    }
+  function getXY(e) {
+    let r = canvas.getBoundingClientRect();
+    return {x: e.clientX - r.left, y: e.clientY - r.top}
+  }
 
-    function getXY(e) {
-      let r = canvas.getBoundingClientRect();
-      return {x: e.clientX - r.left, y: e.clientY - r.top}
-    }
+  function draw(e) {
+    let pos = getXY(e);
 
-    function draw(e) {
-        let pos = getXY(e);
+    if (!painting) return;
+    context.lineWidth = 4;
+    context.lineCap = "round";
+    context.imageSmoothingEnabled = true;
 
-        if (!painting) return;
-        context.lineWidth = 4;
-        context.lineCap = "round";
-        context.imageSmoothingEnabled = true;
+    context.lineTo(e.clientX, e.clientY);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(e.clientX, e.clientY);
 
-        context.lineTo(e.clientX, e.clientY);
-        context.stroke();
-        context.beginPath();
-        context.moveTo(e.clientX, e.clientY);
+    points.push([]);
+    points[points.length - 1].push([pos.x, pos.y]);
 
-        points.push([]);
-        points[points.length - 1].push([pos.x, pos.y]);
-    
-        localStorage.setItem("points", JSON.stringify(points));
-    }
+    localStorage.setItem("points", JSON.stringify(points));
+  }
 
-      canvas.addEventListener("mousedown", startPosition);
-      canvas.addEventListener("mouseup", finishedPosition);
-      canvas.addEventListener("mousemove", draw);
-  });
+  canvas.addEventListener("mousedown", startPosition);
+  canvas.addEventListener("mouseup", finishedPosition);
+  canvas.addEventListener("mousemove", draw);
+});
 
